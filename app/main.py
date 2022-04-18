581ef8bf098c96a6881d014f58a8b3486d60fb77
bartleyg <bartley.gillan@gmail.com>
2022-04-18 10:10:15 -0500

    Cloud Run workaround attempt

--- app/main.py
+++ app/main.py
@@ -5,6 +5,15 @@ import json
 import process
 
 
+# NOTE: Global variable per instance.
+# An unfortunate workaround in Cloud Run with 1 instance to prevent duplicating
+# an activity description update because Cloud Run can take ~10 seconds to run
+# because of cold start plus the processing is limited after the async response
+# to the request is return earlier and the processing here done afterwards.
+# And strava will resend the event to the webhook if the update isn't
+# acknowledged within a couple seconds. So we store the event and don't repeat
+# when it's in already in progress.
+events_in_progress = set()
 app = Quart(__name__)
 
 # webhook route that receives the event data
@@ -13,7 +22,16 @@ async def post_webhook():
     event = await request.get_json()
     print("event received:")
     print(json.dumps(event, indent=4))
+
+    # don't process event if already in progress.
+    activity_id = event["object_id"]
+    if activity_id in events_in_progress:
+        print(f"Event {activity_id} already in progress processing.")
+        return "OK", 200
+    else:
+        events_in_progress.add(activity_id)
     asyncio.create_task(process.process_event(event))
+    events_in_progress.discard(activity_id)
     return "OK", 200
 
 
